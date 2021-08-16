e5deebbc8b23a5878827e1cf95cfd82e9602809c
Nicolle Hernández <87997369+NicolleArevalo2@users.noreply.github.com>
2021-08-15 22:30:51 -0500

    Create capital.py

---
+++ capital.py
@@ -0,0 +1,10 @@
+"""
+entradas
+capital-->float-->cap_invertido
+salidas
+ganancia-->float-->gan
+"""
+cap_invertido=float(input("Digite el capital invertido:"))
+gan=cap_invertido*0.02
+print("el dinero que ganara es:"+str(gan))
+print("Ganancia capital invertido"+str(gan + cap_invertido))
